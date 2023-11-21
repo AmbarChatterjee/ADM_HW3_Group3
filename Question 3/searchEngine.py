e6eb05e8456e41efb19940ab7f4146dcf6f8d04a
Alessandra Colaiocco <146108241+Alessandra9922@users.noreply.github.com>
2023-11-21 14:55:22 +0100

    Add files via upload

--- Question 3/searchEngine.py
+++ Question 3/searchEngine.py
@@ -1,5 +1,7 @@
 import nltk
+import heapq
 import numpy as np
+import pandas as pd
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from sklearn.metrics.pairwise import cosine_similarity
